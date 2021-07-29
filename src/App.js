47bd0e1b8a6db27d819168d5e1e6a485504cd5c9
your_username <amazo.edu@gmail.com>
2021-07-29 21:14:13 +0500

    Component.js created and stylized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CartItem from './CartItem'
 
 function App() {
   return (
     <div className="App">
       <h1>CART</h1>
+      <CartItem />
     </div>
   );
 }
