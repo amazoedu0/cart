63abef2e087315ca6df259e95ff2583dc341f5f9
your_username <amazo.edu@gmail.com>
2021-07-29 21:30:34 +0500

    Constructor Created

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,21 +1,33 @@
 import React from 'react'
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render() {
+        const {price,title,qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image}/>
+                    <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style = {{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs. 999</div>
-                    <div style={{color:'#777'}}>Qty : 1</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs {price}</div>
+                    <div style={{ color: '#777' }}>Qty: {qty}</div>
+                    <div className="cart-item-actions">
+                        {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png"></img>
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png"></img>
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png"></img>
+                    </div>
                 </div>
-                <div className="cart-item-actions">
-                    {/* Buttons */}
-                </div>
-
             </div>
         );
     }
